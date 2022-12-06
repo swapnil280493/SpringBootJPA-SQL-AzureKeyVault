c0c394ab268c16b40d831cc5403afec7d418651e
swapnil280493 <swapnildhage01@gmail.com>
2022-12-06 05:18:13 -0800

    initial commit

---
+++ src/main/java/com/example/SpringJpAwithSqLwithAzureKeyVaultApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpAwithSqLwithAzureKeyVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpAwithSqLwithAzureKeyVaultApplication.class, args);
+	}
+
+}
